22b5c9514f89de8d96b8aaf2bdfeab4aefff39fd
bluedawn123 <haemilyjh@naver.com>
2024-12-27 20:10:08 +0900

    cors 옵션 추가

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const port = 8000
+
+var corsOptions = {
+  //origin: 'http://localhost:3000/',
+  origin: '*' //모든 출처 허용
+}
+app.use(cors(corsOptions));
+
+
 const mysql = require('mysql')
 const db = mysql.createConnection({
   host: 'localhost',
